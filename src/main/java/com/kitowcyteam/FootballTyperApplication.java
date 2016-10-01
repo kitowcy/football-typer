96b43e696eaedd9459267725736b1839636326ee
Przemysław Wojtków <przemek.wojtkow@hotmail.com>
2016-10-01 17:04:48 +0200

    init commit

---
+++ src/main/java/com/kitowcyteam/FootballTyperApplication.java
@@ -0,0 +1,12 @@
+package com.kitowcyteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballTyperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballTyperApplication.class, args);
+	}
+}
